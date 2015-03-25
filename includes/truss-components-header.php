15fe5c7143ddd660970b816f3f0ebac38171ebb0
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2015-03-25 15:51:41 -0700

    Cleanup

--- includes/truss-components-header.php
+++ includes/truss-components-header.php
@@ -47,7 +47,7 @@ function truss_primary_nav() { ?>
 		<?php 	wp_nav_menu( array(
 			    'theme_location' => 'primary-navigation',
 			    'menu_class' => 'flexnav', //Adding the class for FlexNav
-			    'items_wrap' => '<ul data-breakpoint=" '.  get_theme_mod( 'neat_mobile_min_width' ) .' " id="%1$s" class="%2$s">%3$s</ul>', // Adding data-breakpoint for FlexNav
+			    'items_wrap' => '<ul data-breakpoint="'.  get_theme_mod( 'flexnav_breakpoint', '800' ) .'" id="%1$s" class="%2$s">%3$s</ul>', // Adding data-breakpoint for FlexNav
 			    ));
 		?>
 
