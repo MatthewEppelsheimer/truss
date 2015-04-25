7f273e64321d2e134f98ef0db7c40715d5c26ccb
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2015-04-25 13:03:38 +0000

    Add internal hooks to default widgets

--- includes/truss-components-sidebar-primary-static.php
+++ includes/truss-components-sidebar-primary-static.php
@@ -2,7 +2,7 @@
 /**
  * Components intended to be hooked to `truss_sidebar_primary_static`
  *
- * @todo add hooks for each widget: before, inside top, inside bottom, and after
+ * @todo break widget contents into functions and hook to their internal actions
  *
  * @package truss
  */
@@ -16,13 +16,26 @@ add_action( 'truss_sidebar_primary_static', 'truss_default_widget_meta', 300 );
  *
  * Intended to be hooked to `truss_default_sidebar_primary_static`
  *
+ * Fires hooks:
+ *   - truss_default_widget_search_before
+ *   - truss_default_widget_search_top
+ *   - truss_default_widget_search_bottom
+ *   - truss_default_widget_search_after
+ *
  * @package truss
  */
-function truss_default_widget_search() { ?>
+function truss_default_widget_search() {
+	do_action( 'truss_default_widget_search_before' ); ?>
 	<aside class="widget<?php echo apply_filters( 'truss_class_widget-search', ' widget-search' ); ?>">
-		<?php get_search_form(); ?>
+
+		<?php
+		do_action( 'truss_default_widget_search_top' );
+		get_search_form();
+		do_action( 'truss_default_widget_search_bottom' );
+		?>
+
 	</aside>
-<?php
+	<?php do_action( 'truss_default_widget_search_after' );
 }
 
 /**
@@ -30,16 +43,27 @@ function truss_default_widget_search() { ?>
  *
  * Intended to be hooked to `truss_default_sidebar_primary_static`
  *
+ * Fires hooks:
+ *   - truss_default_widget_archives_before
+ *   - truss_default_widget_archives_top
+ *   - truss_default_widget_archives_bottom
+ *   - truss_default_widget_archives_after
+ *
  * @package truss
  */
-function truss_default_widget_archives() { ?>
+function truss_default_widget_archives() {
+	do_action( 'truss_default_widget_archives_before' ); ?>
 	<aside class="widget<?php echo apply_filters( 'truss_class_widget-archives', ' widget-archives' ); ?>">
+		<?php do_action( 'truss_default_widget_archives_top' ); ?>
+
 		<h4 class="<?php echo apply_filters( 'truss_class_widget-title', 'widget-title' ); ?>"><?php _e( 'Archives', 'truss' ); ?></h4>
 		<ul>
 			<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 		</ul>
+
+		<?php do_action( 'truss_default_widget_archives_bottom' ); ?>
 	</aside>
-<?php
+	<?php do_action( 'truss_default_widget_archives_after' );
 }
 
 /**
@@ -47,16 +71,27 @@ function truss_default_widget_archives() { ?>
  *
  * Intended to be hooked to `truss_default_sidebar_primary_static`
  *
+ * Fires hooks:
+ *   - truss_default_widget_meta_before
+ *   - truss_default_widget_meta_top
+ *   - truss_default_widget_meta_bottom
+ *   - truss_default_widget_meta_after
+ *
  * @package truss
  */
-function truss_default_widget_meta() { ?>
+function truss_default_widget_meta() {
+	do_action( 'truss_default_widget_meta_before' );?>
 	<aside class="widget<?php echo apply_filters( 'truss_class_widget-meta', ' widget-meta' ); ?>">
+
+		<?php do_action( 'truss_default_widget_meta_top' ); ?>
 		<h4 class="<?php echo apply_filters( 'truss_class_widget-title', 'widget-title' ); ?>"><?php _e( 'Meta', 'truss' ); ?></h4>
 		<ul>
 			<?php wp_register(); ?>
 			<li><?php wp_loginout(); ?></li>
-			<?php wp_meta(); ?>
+			<?php wp_meta();
+
+			do_action( 'truss_default_widget_meta_bottom' ); ?>
 		</ul>
 	</aside>
-<?php
+	<?php do_action( 'truss_default_widget_meta_after' );
 }
