1a7bdc4922d8820454ef715f83a1012aaa09cbcd
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2015-04-25 12:48:20 +0000

    Change primary sidebar wrapper from <div> to <section>

--- includes/truss-components-sidebar-primary.php
+++ includes/truss-components-sidebar-primary.php
@@ -13,24 +13,23 @@ add_action( 'truss_sidebar_primary', 'truss_layout_sidebar_primary', 100 );
  * Hooks fired:
  *   - tha_sidebars_before
  *   - tha_sidebar_top
+ *   - truss_sidebar_primary_static
  *   - tha_sidebar_bottom
  *   - tha_sidebars_after
- *
- * @todo audit all markup
  *
  * @package truss
  */
 function truss_layout_sidebar_primary() {
 	tha_sidebars_before(); ?>
-	<div class="<?php echo apply_filters( 'truss_class_sidebar_primary', 'widget-area secondary' ); ?>" role="complementary">
+	<section class="<?php echo apply_filters( 'truss_class_sidebar_primary', 'widget-area secondary' ); ?>" role="complementary">
 		<?php
 		tha_sidebar_top();
+
 		if ( ! dynamic_sidebar( 'sidebar-1' ) ) {
-
 			truss_sidebar_primary_static();
-
 		}
+
 		tha_sidebar_bottom(); ?>
-	</div>
+	</section>
 <?php tha_sidebars_after();
 }
