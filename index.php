6d6089b0ccc0b8a3ce838d4f17e3d75bf03e15ef
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2015-06-23 22:29:52 +0200

    Finalize idealized index.php markup

--- index.php
+++ index.php
@@ -5,17 +5,9 @@
  * @package truss
  */
 
-// Build layout: A primary column and a right-hand sidebar
-// This is what this is intended to ultimately look like
-/*
-add_action( 'truss', 'truss_main_column' );
-add_action( 'truss', 'truss_right_sidebar', 300 );
-
-// Strong as steel.
-truss();
-*/
-
-add_action( 'truss', 'truss_main', 200 );
+// Layout: A primary column and a right-hand sidebar column
+add_action( 'truss', 'truss_column_primary', 200 );
 add_action( 'truss', 'truss_sidebar_primary', 300 );
 
+// Strong as steel.
 truss();
