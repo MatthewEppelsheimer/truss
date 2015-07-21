bf1b3da53a39684b42e386b980b8ebf7f8e29b1b
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2015-07-21 07:13:36 -0700

    Fix JavaScript but (wrap in <script> tags)

--- library/includes/truss-components-wp-footer.php
+++ library/includes/truss-components-wp-footer.php
@@ -18,9 +18,10 @@ add_action( 'wp_footer', 'truss_document_ready', 100 );
  */
 function truss_document_ready() {
 	if ( has_action( 'truss_document_ready' ) ) { ?>
-		jQuery( document ).ready(function(){
-			<?php truss( 'document_ready' ); ?>
-		});
+		<script type="text/javascript">
+			jQuery( document ).ready(function(){
+				<?php truss( 'document_ready' ); ?>
+			});
+		</script>
 	<?php }
 }
-
